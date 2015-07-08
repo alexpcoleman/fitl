55500dfdcbdde88ae8de416fa4129c3fe2230a14
Alex Coleman <me@alexpcoleman.com>
2015-07-08 13:17:15 -0500

    initial laravel application with a static page controller and about page

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>About</title>
+	</head>
+	<body>
+		<h1>About</h1>
+		<p>This is the application I'm building in the <a href="https://selftaughtcoders.com/from-idea-to-launch">From Idea To Launch</a> course.</p>
+		<p>This web application will allow people to both submit, and answer, programming-related questions for various different programming languages.</p>
+	</body>
+</html>
